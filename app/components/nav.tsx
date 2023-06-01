77da832b1a35dc52b445b9ea2b36e671d920f9e9
Sodasom <1219dasom@gmail.com>
2023-06-01 14:10:42 +0900

    design : header, nav 내용 추가

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,3 +1,41 @@
 export default function Nav() {
-  return <nav>Nav</nav>;
+  return (
+    <nav>
+      <ul className="flex">
+        <li className="">Accordion</li>
+        <li>Bento Menu</li>
+        <li>Breadcrumb</li>
+        <li>Button</li>
+        <li>Card</li>
+        <li>Carousel</li>
+        <li>Checkbox</li>
+        <li>Comment</li>
+        <li>Doner Menu</li>
+        <li>Dropdown</li>
+        <li></li>
+        <li>Feed</li>
+        <li>Form</li>
+        <li>Hamberger Menu</li>
+        <li>Icon</li>
+        <li>Input Field</li>
+        <li>Kebab Menu</li>
+        <li>Loader</li>
+        <li>Meatballs Menu</li>
+        <li>Modal</li>
+        <li>Badge</li>
+        <li>Pagenation</li>
+        <li>Picker</li>
+        <li>Progress Bar</li>
+        <li>Radio Buttons</li>
+        <li>Search Field</li>
+        <li>Sidebar</li>
+        <li>Slider Controls</li>
+        <li>Stepper</li>
+        <li>Tag</li>
+        <li>Tab Bar</li>
+        <li>Tooltip</li>
+        <li>Toggle</li>
+      </ul>
+    </nav>
+  );
 }
