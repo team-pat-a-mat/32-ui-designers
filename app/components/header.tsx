21d9c8e98b13c875464f47023917b84a4755dcb1
Sodasom <1219dasom@gmail.com>
2023-06-01 14:11:13 +0900

    design : header, nav 내용 설정

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,9 @@
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <h1 className="text-[32px] font-bold">
+        32 User Interface Elements For UI Designers
+      </h1>
+    </header>
+  );
 }
